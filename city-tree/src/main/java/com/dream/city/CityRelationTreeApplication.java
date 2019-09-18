3dba1157202c90b44270a0c54b2eb57270f5271b
Wvv <aoswce@126.com>
2019-09-18 15:11:31 +0800

    修改auth和添加tree

--- city-tree/src/main/java/com/dream/city/CityRelationTreeApplication.java
+++ city-tree/src/main/java/com/dream/city/CityRelationTreeApplication.java
@@ -6,14 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author Wvv
+ */
 @SpringBootApplication
 @EnableCaching
 @EnableEurekaClient
 @MapperScan("com.dream.city.domain.mapper")
-public class CityAuthApplication {
+public class CityRelationTreeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CityAuthApplication.class, args);
+        SpringApplication.run(CityRelationTreeApplication.class, args);
     }
 
 }
