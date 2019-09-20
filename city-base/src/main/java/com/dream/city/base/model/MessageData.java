a3f9d6e1a884d6423bf2272137e68767dcf30382
liunh1220 <liunh120@sina.cn>
2019-09-20 18:41:14 +0800

    投资项目

--- city-base/src/main/java/com/dream/city/base/model/MessageData.java
+++ city-base/src/main/java/com/dream/city/base/model/MessageData.java
@@ -25,4 +25,28 @@ public class MessageData<T> {
         this.model = model;
         this.t = t;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
 }
