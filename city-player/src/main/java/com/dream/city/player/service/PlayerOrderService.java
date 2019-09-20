d6e639822c5ad5fdd225f17d0069f0caf3e65a2d
lnh1220 <lnh120@sina.cn>
2019-09-20 15:19:08 +0800

    投资订单

---
+++ city-player/src/main/java/com/dream/city/player/service/PlayerOrderService.java
@@ -0,0 +1,11 @@
+package com.dream.city.player.service;
+
+/**
+ * 玩家订单
+ */
+public interface PlayerOrderService {
+
+
+
+
+}
---
+++ city-player/src/main/java/com/dream/city/player/service/impl/InvestOrderServiceImpl.java
@@ -0,0 +1,52 @@
+package com.dream.city.player.service.impl;
+
+import com.dream.city.base.model.entity.InvestOrder;
+import com.dream.city.base.model.enu.OrderState;
+import com.dream.city.player.domain.mapper.InvestOrderMapper;
+import com.dream.city.player.service.InvestOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InvestOrderServiceImpl implements InvestOrderService {
+
+
+    @Autowired
+    private InvestOrderMapper orderMapper;
+
+
+
+    @Override
+    public int insertInvestOrder(InvestOrder record) {
+        Integer integer = orderMapper.updateByPrimaryKeySelective(record);
+        return integer ==null?0:integer;
+    }
+
+    @Override
+    public int investOrderInvalid(InvestOrder record) {
+        record.setOrderState(OrderState.INVALID.getStatus());
+        Integer integer = orderMapper.updateByPrimaryKeySelective(record);
+        return integer ==null?0:integer;
+    }
+
+    @Override
+    public int investOrderCancel(InvestOrder record) {
+        record.setOrderState(OrderState.CANCEL.getStatus());
+        Integer integer = orderMapper.updateByPrimaryKeySelective(record);
+        return integer ==null?0:integer;
+    }
+
+    @Override
+    public InvestOrder getInvestOrder(InvestOrder record) {
+        return orderMapper.selectByPrimaryKey(record);
+    }
+
+    @Override
+    public List<InvestOrder> getInvestOrders(InvestOrder record) {
+        return orderMapper.getInvestOrders(record);
+    }
+
+
+}
