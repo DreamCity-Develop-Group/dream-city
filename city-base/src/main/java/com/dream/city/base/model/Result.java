cb98650048c9c2c100155de574e79471794968dd
lnh1220 <lnh120@sina.cn>
2019-09-21 11:36:47 +0800

    Result构造函数

--- city-base/src/main/java/com/dream/city/base/model/Result.java
+++ city-base/src/main/java/com/dream/city/base/model/Result.java
@@ -76,6 +76,16 @@ public class Result<T> {
         }
     }
 
+    public Result(String msg, int code, T data) {
+        super();
+        this.msg = msg;
+        this.code = code;
+        this.data = data;
+        if (code == CityGlobal.ResultCode.success.getStatus()){
+            this.success = Boolean.TRUE;
+        }
+    }
+
     public Result(boolean success, String msg, int code, T data) {
         super();
         this.success = success;
