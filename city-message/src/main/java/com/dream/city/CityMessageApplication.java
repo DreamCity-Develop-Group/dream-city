141d71c9e3db6a7e6176f609ad8cf6c7b7fe7013
liunh1220 <liunh120@sina.cn>
2019-09-26 16:43:54 +0800

    修改@Import({RedisConfig.class})

--- city-message/src/main/java/com/dream/city/CityMessageApplication.java
+++ city-message/src/main/java/com/dream/city/CityMessageApplication.java
@@ -1,10 +1,12 @@
 package com.dream.city;
 
+import com.dream.city.base.config.RedisConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author Wvv
@@ -13,6 +15,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @MapperScan("com.dream.city.domain.mapper")
 @EnableCaching
+@Import({RedisConfig.class})
 public class CityMessageApplication {
 
     public static void main(String[] args) {
--- city-worker/src/main/java/com/dream/city/CityWorkerApplication.java
+++ city-worker/src/main/java/com/dream/city/CityWorkerApplication.java
@@ -1,9 +1,11 @@
 package com.dream.city;
 
+import com.dream.city.base.config.RedisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author Wvv
@@ -11,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableCaching
 @EnableEurekaClient
+@Import({RedisConfig.class})
 public class CityWorkerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CityWorkerApplication.class,args);
