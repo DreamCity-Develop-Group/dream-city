68bf09bbf0fa62bdd69e2fd686f8849f7c1ca4e3
liunh1220 <547119066@qq.com>
2019-10-01 20:30:31 +0800

    合并代码

--- city-worker/src/main/java/com/dream/city/job/InvestOrderJob.java
+++ city-worker/src/main/java/com/dream/city/job/InvestOrderJob.java
@@ -20,7 +20,7 @@ public class InvestOrderJob extends QuartzJobBean {
     private UserServiceImpl userService;
 
     /**
-     * 
+     *
      *
      * @param jobExecutionContext
      * @throws JobExecutionException
