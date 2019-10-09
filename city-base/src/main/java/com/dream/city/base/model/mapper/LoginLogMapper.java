9598525995bf06c4b285bf3d24e3947ce9399221
liunh1220 <547119066@qq.com>
2019-10-09 16:37:42 +0800

    后台玩家登陆日志

--- city-base/src/main/java/com/dream/city/base/model/mapper/LoginLogMapper.java
+++ city-base/src/main/java/com/dream/city/base/model/mapper/LoginLogMapper.java
@@ -2,16 +2,23 @@ package com.dream.city.base.model.mapper;
 
 
 import com.dream.city.base.model.entity.LoginLog;
+import com.dream.city.base.model.req.LoginLogReq;
+import com.dream.city.base.model.resp.LoginLogResp;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface LoginLogMapper {
+
     int deleteByPrimaryKey(Long id);
 
     int insertSelective(LoginLog record);
 
-    LoginLog selectByPrimaryKey(Long id);
+    LoginLogResp selectByPrimaryKey(Long id);
 
     int updateByPrimaryKeySelective(LoginLog record);
+
+    List<LoginLogResp> getLoginLogList(LoginLogReq record);
 
 }
