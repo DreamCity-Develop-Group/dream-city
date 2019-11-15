1832a1b50ebbd11186dc6a21728ec64b6a1be641
894346784@qq.com <894346784@qq.com>
2019-11-15 23:50:23 +0800

    发放收益

---
+++ city-worker/src/main/java/com/dream/city/job/ProfitGrantJobReload.java
@@ -0,0 +1,46 @@
+package com.dream.city.job;
+
+import com.dream.city.base.utils.RedisUtils;
+import com.dream.city.service.*;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * TODO 核算 玩家每日收益结果
+ *
+ * @author WVv
+ */
+@Slf4j
+public class ProfitGrantJobReload extends QuartzJobBean {
+
+    @Autowired
+    InvestService investService;
+
+    @Autowired
+    RedisUtils redisUtils;
+
+    /**
+     * TODO
+     * 1、取出所有投资项目
+     * 2、遍历所有投资项目预约的（到截止时间，上一工作日的所有投资订单）
+     * 3、在2中，直接根据规则返回所有能够取得成功的成员
+     * 4、遍历所有成功的订单，设置为成功状态
+     *
+     * @param jobExecutionContext
+     * @throws JobExecutionException
+     */
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        investService.profitGrant();
+    }
+
+}
