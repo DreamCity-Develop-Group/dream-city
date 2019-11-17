1b7450e5ccc77d2fbc3a6d4e3ed9d1b57ba36e44
894346784@qq.com <894346784@qq.com>
2019-11-17 16:47:45 +0800

    发放收益

--- city-worker/src/main/java/com/dream/city/job/ProfitCalculateJob.java
+++ city-worker/src/main/java/com/dream/city/job/ProfitCalculateJob.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 
 /**
@@ -64,35 +65,49 @@ public class ProfitCalculateJob extends QuartzJobBean {
             //直接找出成功的投资订单，计算新增的资金额度
             List<InvestOrder> orders = orderService.getInvestOrdersAmountByDayInterval(invest.getInId(), start, end);
             //计算总的新增资金总额度
-            System.out.println("orders="+orders);
             BigDecimal total = BigDecimal.ZERO;
             for (InvestOrder order : orders) {
                 total = total.add(order.getOrderAmount());
             }
 
-            //平均分成15份
-            BigDecimal average = total.divide(new BigDecimal(15));
-            BigDecimal[] averages = new BigDecimal[15];
-            for (int i = 0; i < 15; i++) {
-                double x = Math.floor(Math.random() * 10);
-                double y = x / 40;
-                System.out.println(y);
-                BigDecimal subtract = average.multiply(new BigDecimal(y));
-                averages[i] = subtract.add(average);
-                averages[i + 7] = average.subtract(subtract);
-                if (i==7){
-                    break;
+            BigDecimal temp = BigDecimal.ZERO;//累计已添加到队列的收益
+            BigDecimal rate = new BigDecimal(0.7);//随机数
+            BigDecimal onceProfit = BigDecimal.ZERO;//每次添加的收益
+            for (int i = 0; i < 10; i++) {
+                rate = new BigDecimal(Math.random()+0.5).setScale(1, RoundingMode.HALF_UP);
+                onceProfit = total.multiply(rate);
+                if(i==9){
+                    redisUtils.lpush(ProfitQueue+"_"+invest.getInId(),String.valueOf(total.subtract(temp)));
+                }else{
+                    redisUtils.lpush(ProfitQueue+"_"+invest.getInId(),String.valueOf(onceProfit));
+                    temp = temp.add(onceProfit);
                 }
             }
+
+
+//            //平均分成15份
+//            BigDecimal average = total.divide(new BigDecimal(10));
+//            BigDecimal[] averages = new BigDecimal[10];
+//            for (int i = 0; i < 15; i++) {
+//                double x = Math.floor(Math.random() * 10);
+//                double y = x / 40;
+//                System.out.println(y);
+//                BigDecimal subtract = average.multiply(new BigDecimal(y));
+//                averages[i] = subtract.add(average);
+//                averages[i + 7] = average.subtract(subtract);
+//                if (i==7){
+//                    break;
+//                }
+//            }
             /**
              *TODO
              * 将计算结果放入 Redis队列 ProfitQueue
              */
 
-            for(int i=0;i<averages.length;i++){
-                System.out.println("i="+i+","+averages[i]);
-                redisUtils.lpush(ProfitQueue+"_"+invest.getInId(),String.valueOf(averages[i]));
-            }
+//            for(int i=0;i<averages.length;i++){
+//                System.out.println("i="+i+","+averages[i]);
+//                redisUtils.lpush(ProfitQueue+"_"+invest.getInId(),String.valueOf(averages[i]));
+//            }
 
         });
     }
